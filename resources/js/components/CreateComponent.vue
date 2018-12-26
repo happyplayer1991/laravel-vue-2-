0d83296e513f876bbef5bc5540c1776ca0116491
freshcoderchan <freshcoderjack@outlook.com>
2018-12-26 08:27:55 -0800

    Make Vue compoent

---
+++ resources/js/components/CreateComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card card-default">
+        <div class="card-header">Create Component</div>
+        <div class="card-body">I am the Create Component Compoent</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Create Component mounted.')
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/IndexComponet.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card card-default">
+        <div class="card-header">Index Component</div>
+        <div class="card-body">I am the Index Component Compoent</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Index Component mounted.')
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
