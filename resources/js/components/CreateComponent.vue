3449be0e0df99c5299c6a753f95c7612bd7d7f7a
freshcoderchan <freshcoderjack@outlook.com>
2018-12-26 13:52:12 -0800

    Create Post

--- resources/js/components/CreateComponent.vue
+++ resources/js/components/CreateComponent.vue
@@ -1,19 +1,42 @@
 <template>
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card card-default">
-        <div class="card-header">Create Component</div>
-        <div class="card-body">I am the Create Component Compoent</div>
+  <div>
+    <h1>Create A Post</h1>
+    <form @submit.prevent="addPost">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="">Post Title:</label>
+            <input type="text" class="form-control" v-model="post.title">
+          </div>
+        </div>
       </div>
-    </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="">Post Body:</label>
+            <textarea class="form-control" v-model="post.body" rows="5"></textarea>
+          </div>
+        </div>
+      </div><br />
+      <div class="form-group">
+        <button class="btn btn-primary">Create</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
   export default {
-    mounted() {
-      console.log('Create Component mounted.')
-    }
+    data() {
+      return {
+        post: {}
+      }
+    },
+    methods: {
+      addPost() {
+        console.log(this.post);
+      }
+    },
   }
 </script>
 
